b559002bb984bc4a09d5f9b7776d8460dc00fc72
Max <maximumz160@gmail.com>
2025-04-29 13:26:53 +0700

    Fix 404, fallback to React Router

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
       <Footer />
     </>
