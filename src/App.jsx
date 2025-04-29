2b20afbb7b1f9350c63c4560b99e85382d84a8cf
Max <maximumz160@gmail.com>
2025-04-29 13:38:51 +0700

    Fixx not found page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import NotFoundPage from './components/NotFoundPage';
 
 function App() {
   return (
